1da61c8a65f1cf6622871a4bc60fae990e751521
jacob13 <jacob13foster@outlook.com>
2025-04-11 15:22:59 -0500

    extract and format data from api results

--- kafka_stream.py
+++ kafka_stream.py
@@ -11,10 +11,38 @@ default_args = {
 }
 
 # create function that will be called in the Airflow DAG to pipe data from the API 
-def stream_data():
+def get_data():
     res = requests.get('https://randomuser.me/api/')
-    data = res.json()['results'][0]
-    print(data)
+    res = res.json()['results'][0]
+
+    return res
+
+# need to format the data in a specific manner
+# task needs to be added to the Kafka queue so must have the  decorator on it
+def format_data(res):
+    user_data = {}
+
+    # data extraction from JSON object
+    user_data['first_name'] = res['name']['first']
+    user_data['last_name'] = res['name']['last']
+    user_data['gender'] = res['gender']
+    user_data['zipcode'] = res['location']['postcode']
+    user_data['email '] = res['email']
+    user_data['username'] = res['login']['username']
+    user_data['password'] = res['login']['sha256']
+    user_data['salt'] = res['login']['salt']
+    user_data['dob'] = res['dob']['date']
+    user_data['registration_date'] = res['registered']['date']
+    user_data['phone'] = res['phone']
+    user_data['photo'] = res['picture']['medium']
+
+    return user_data
+
+
+def stream_data():
+    res = get_data()
+    res = format_data(res)    
+    print(res)
 
 
 # create a DAG (Directed Acyclic Graph) that triggers tasks in sequential order
