52f4c0ef81b57c481c74961fdfc198c2df847ba1
jacob13 <jacob13foster@outlook.com>
2025-04-11 20:33:07 -0500

    added script for manually installing docker and specified docker image

---
+++ docker_install_linux.sh
@@ -0,0 +1,10 @@
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# run these later -- gets finnicky if you run them too soon
+# sudo systemctl restart docker
+# sudo systemctl enable docker
